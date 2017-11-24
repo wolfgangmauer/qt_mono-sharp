4d047b57d43cdf8b4ee468b71b4d21f9565dec1f
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2017-11-24 20:08:56 +0100

    Add files via upload

---
+++ example/MyMainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Qt;
+using System.Diagnostics;
+
+namespace TestQtMonoSharp
+{
+	public class MyMainWindow : Qt.MainWindow
+	{
+		public MyMainWindow (Qt.Widget parent = null)
+			: base(parent)
+		{
+			KeyPressEvent += MyMainWindow_KeyPressEvent;
+		}
+
+		void MyMainWindow_KeyPressEvent (object sender, KeyEventArgs e)
+		{
+		}
+	}
+}
+
---
+++ example/QMessagePanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Qt;
+using System.IO;
+using System.Reflection;
+
+namespace TestQtMonoSharp
+{
+	public class MessagePanel : Dialog
+	{
+		[UiLoaderAttribute("CAPTION")]
+		Label _caption;
+
+		[UiLoaderAttribute("NOW")]
+		Label _now;
+
+		[UiLoaderAttribute("NEXT")]
+		Label _next;
+
+		[UiLoaderAttribute("REDBUTTON")]
+		Label _redlabel;
+
+		[UiLoaderAttribute()]
+		Label GREENBUTTON;
+
+		[UiLoaderAttribute()]
+		Label YELLOWBUTTON;
+
+		[UiLoaderAttribute()]
+		Label BLUEBUTTON;
+
+		public MessagePanel (Qt.Widget parent = null) : base(IntPtr.Zero)
+		{
+			Raw = new UiLoader(parent).Load( uiFile, this);
+
+			_caption.Text = "Caption";
+			_redlabel.Text = "Nein";
+			GREENBUTTON.Text = "Ja";
+			YELLOWBUTTON.Text = string.Empty;
+			BLUEBUTTON.Text ="Optionen";
+		}
+	}
+}
+
