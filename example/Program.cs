f76f65faa0649cfbae8ed355d85bed2554de7f16
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2017-11-24 20:07:32 +0100

    Create Program.cs

---
+++ example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Qt;
+using System.Threading;
+
+namespace TestQtMonoSharp
+{
+	class MainClass
+	{
+		delegate void HideFromJit(string[] args);
+
+		public static void Main (string[] args)
+		{
+			HideFromJit d = StartUp;
+			d (args);
+		}
+
+		public static void StartUp(string[] args)
+		{
+			GLib.Global.ProgramName = "ProgramName";
+
+			CoreApplication.InitMonoInternal ();
+
+			Application.Attribute = ApplicationAttribute.AA_EnableHighDpiScaling;
+
+			var app = new Application (args);
+
+			var mainWindow = new MyMainWindow ();
+			mainWindow.FixedSize = new Size(1920/2, 1080/2);
+			mainWindow.Show ();
+
+			var splash = new MessagePanel (mainWindow);
+			splash.Exec ();
+
+			app.Exec ();
+		}
+	}
+}
