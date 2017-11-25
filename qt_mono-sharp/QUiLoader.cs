e637c21c0b5081d24315f094916242fd672b3057
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2017-11-25 03:43:25 +0100

    Add files via upload

--- qt_mono-sharp/QUiLoader.cs
+++ qt_mono-sharp/QUiLoader.cs
@@ -12,19 +12,21 @@ namespace Qt
 
 		protected UiLoader (IntPtr raw) : base(raw) {}
 
-		public UiLoader (Object parent = null) : base(IntPtr.Zero)
+        Widget _parent; 
+		public UiLoader (Widget parent) : base(IntPtr.Zero)
 		{
+            _parent = parent;
 			Raw = qt_uiloader_new (this, parent != null ? parent.Handle : IntPtr.Zero);
 		}
 
 		[MethodImpl(MethodImplOptions.InternalCall)]
 		protected static extern IntPtr qt_uiloader_load (IntPtr handle, string uiFile, IntPtr parentWidget);
-		public IntPtr Load(string uiFile, Widget parentWidget = null)
+		public IntPtr Load(string uiFile)
 		{
-			var retVal = qt_uiloader_load (Handle, uiFile, parentWidget != null ? parentWidget.Handle : IntPtr.Zero);			
-			if (parentWidget != null)
+			var retVal = qt_uiloader_load (Handle, uiFile, _parent != null ? _parent.Handle : IntPtr.Zero);			
+			if (_parent != null)
 			{
-				var fields4Loader = parentWidget.GetType().GetFields(BindingFlags.NonPublic| BindingFlags.Instance).Where(field => field.IsDefined(typeof(UiLoaderAttribute), false));
+				var fields4Loader = _parent.GetType().GetFields(BindingFlags.NonPublic| BindingFlags.Instance).Where(field => field.IsDefined(typeof(UiLoaderAttribute), false));
 				foreach (var field in fields4Loader)
 				{
 					var fieldAttribute = (UiLoaderAttribute)field.GetCustomAttribute (typeof(UiLoaderAttribute));
@@ -34,7 +36,7 @@ namespace Qt
 					var rawObject = FindRawObject (retVal, fieldAttrName);
 					if (rawObject != IntPtr.Zero)
 					{
-						field.SetValue (parentWidget, Activator.CreateInstance (field.FieldType, BindingFlags.CreateInstance | BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] { rawObject }, null));
+						field.SetValue (_parent, Activator.CreateInstance (field.FieldType, BindingFlags.CreateInstance | BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] { rawObject }, null));
 					}
 				}
 			}
